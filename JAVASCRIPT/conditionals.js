2686b2f3907c57aeda7d217511e33cec215de740
Muhammad Husnain <husnainbhinder682@gmail.com>
2024-12-13 14:39:03 +0500

    updated files

---
+++ JAVASCRIPT/conditionals.js
@@ -0,0 +1,30 @@
+
+let marks = 88;
+
+if (marks == 40) {
+    console.log("you have passed the course");
+    
+} 
+else if(marks < 40){
+    console.log("you have failed the course");
+
+} 
+else if (marks >= 50 && marks <=59) {
+    console.log("you have got E grade"); 
+}
+
+else if (marks >= 60 && marks <= 69) {
+    console.log("you have got D grade");
+}
+else if (marks >= 70 && marks <= 79) {
+    console.log("you have got C grade");
+}
+else if (marks >= 80 && marks <= 89) {
+    console.log("you have got B grade");
+}
+else if (marks >= 90) {
+    console.log("you have got A grade");
+}
+
+
+
--- JS/sigmawebdev-JS/conditionals.js
+++
@@ -1,9 +0,0 @@
-let marks = 55;
-
-if (marks > 40) {
-    console.log("you have passed the course");
-    
-} else if(marks < 40){
-    console.log("you have failed the course");
-    
-}
