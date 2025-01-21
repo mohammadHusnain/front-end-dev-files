4186cc6d44901b3cc8ad3e283e219d1fdcb12203
Muhammad Husnain <husnainbhinder682@gmail.com>
2025-01-21 11:45:49 +0500

    updated files

--- JAVASCRIPT/exercises/exercises.js
+++ JAVASCRIPT/exercises/exercises.js
@@ -70,30 +70,51 @@
 
 // exercise 3 :
 
-function factorial(num) {
-    if (num == 0 || num == 1) {
-        return 1;
-    }
+// function factorial(num) {
+//     if (num == 0 || num == 1) {
+//         return 1;
+//     }
 
-    let result = 1;
+//     let result = 1;
 
-    for (let index = 1; index <= num; index++) {
-        result = result*index;
-    }
-    return result;
-}
-const num = 5;
-console.log("factorial of "+ num + " is : " + factorial(num));
+//     for (let index = 1; index <= num; index++) {
+//         result = result*index;
+//     }
+//     return result;
+// }
+// const num = 5;
+// console.log("factorial of "+ num + " is : " + factorial(num));
 
-function fact(numb) {
-    if (numb === 0 || numb === 1) {
-        return 1;
-    }
-    return Array.from({ length: numb }, (_, i) => i + 1).reduce((result, index) => result * index, 1);
-}
+// function fact(numb) {
+//     if (numb === 0 || numb === 1) {
+//         return 1;
+//     }
+//     return Array.from({ length: numb }, (_, i) => i + 1).reduce((result, index) => result * index, 1);
+// }
 
-const numb = 6; // Replace with your desired number
-console.log("Factorial of " + numb + " is: " + factorial(numb));
+// const numb = 6; // Replace with your desired number
+// console.log("Factorial of " + numb + " is: " + factorial(numb));
 
 //exercise 4
 
+console.log("4 th exercise showing giving random colors to boxes using dom manipulation");
+
+// let boxes = document.getElementsByClassName("box");
+let boxes = document.querySelector(".container").children //same as above acccess the all children of container which are boxes
+
+console.log(boxes);
+
+
+//a+Math.random() (b-a) a=0 , b=255;   logic
+function getrandomclr(){        
+    let val1 = 0 + Math.random() * 255
+    let val2 = 0 + Math.random() * 255;
+    let val3 = 0 + Math.random() * 255; 
+
+    return `rgb(${val1} , ${val2} , ${val3})`; //template literals for printing values
+}
+
+Array.from(boxes).forEach(e=> {
+e.style.backgroundColor = getrandomclr();    
+e.style.color = getrandomclr();
+})
