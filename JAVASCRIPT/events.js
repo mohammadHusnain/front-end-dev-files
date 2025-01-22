1b6ef9eec7f47626ed3746f2c9dfbdbf21ce082c
Muhammad Husnain <husnainbhinder682@gmail.com>
2025-01-22 17:45:27 +0500

    updated files

--- JAVASCRIPT/events.js
+++ JAVASCRIPT/events.js
@@ -2,6 +2,14 @@ console.log("events and event handling");
 
 let button = document.getElementById("btn");
 
+//  template syntax for writing an event in js
+
+// element.addEventListener("write name of event here wg click, doubleclick, contectmenu etc   " , function example such as () =>{
+//     document.querySelector(".box").innerHTML= "yayu are clicked"
+
+// })
+
+
 button.addEventListener("click" , ()=>{
 document.querySelector(".box").innerHTML = "<b>Yayy u were clicked</b> Enjoy your Click!";
 })
