dc14c399e7a312dcd8dd0ca9180902d85bf192ff
Muhammad Husnain <husnainbhinder682@gmail.com>
2025-01-26 16:00:57 +0500

    updated files

---
+++ JAVASCRIPT/Mini_Projects/p4_GuessTheNumber/script.js
@@ -0,0 +1,10 @@
+const randomNumber = (parseInt(Math.random() * 100 + 1 ));
+
+const submit = document.querySelector('#subt')
+const userInput = document.querySelector('#guessfield')
+const guessSlot = document.querySelector('.guesses')
+const remaining = document.querySelector('.lastresult')
+const lowOrHigh = document.querySelector('.loworhi')
+
+
+
