7a3aaaa5ee0306c4baa9ee5ca51e8dcf5341e48d
Muhammad Husnain <husnainbhinder682@gmail.com>
2025-02-03 12:14:18 +0500

    updated files

--- JAVASCRIPT/SPOTIFY_CLONE/script.js
+++ JAVASCRIPT/SPOTIFY_CLONE/script.js
@@ -1,37 +1,55 @@
 
 
-async function getSongs(){
+async function getSongs() {
     const a = await fetch("http://127.0.0.1:5500/JAVASCRIPT/SPOTIFY_CLONE/songs/")
-    
-let response = await a.text();
-console.log(response);
 
-let div = document.createElement("div");
-div.innerHTML = response;
+    let response = await a.text();
+    console.log(response);
 
-let as = div.getElementsByTagName("a");
+    let div = document.createElement("div");
+    div.innerHTML = response;
+
+    let as = div.getElementsByTagName("a");
 
     let songs = [];
 
-for (let index = 0; index < as.length; index++) {
-    const element = as[index];
-    if(element.href.endsWith(".mp3")){
-        songs.push(element.href)
+    for (let index = 0; index < as.length; index++) {
+        const element = as[index];
+        if (element.href.endsWith(".mp3")) {
+            songs.push(element.href.split("/songs/")[1])
+        }
     }
+
+    return songs;
+
 }
 
-return songs;
+async function main() {
 
-}
-
-async function main(){
-    
     let songs = await getSongs();
     console.log(songs);
 
+// commented code only removed %20 and below code removes all %26 and %2c also so used
+
+    // let songUL = document.querySelector(".songlist").getElementsByTagName("ul")[0]
+    // for (const song of songs) {
+    //     songUL.innerHTML = songUL.innerHTML + `<li> ${song.replaceAll("%20", " ") }</li>`
+    // }
+
+    let songUL = document.querySelector(".songlist").getElementsByTagName("ul")[0];
+
+    for (const song of songs) {
+        let formattedSong = song.replaceAll("%20", " ")
+            .replaceAll("%2C", " ")
+            .replaceAll("%26", " ");
+
+        songUL.innerHTML += `<li>${formattedSong}</li>`;
+    }
+
+
     // play first song
-    var audio = new Audio(songs[]); // index of the song to be played
-    audio.play();
+    var audio = new Audio(songs[0]); // index of the song to be played
+    // audio.play();
 }
 
 main();
