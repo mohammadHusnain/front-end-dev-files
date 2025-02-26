af964b6b3e2f680079b48dc55476955b897592cf
Muhammad Husnain <husnainbhinder682@gmail.com>
2025-02-26 09:12:25 +0500

    updated files

--- REACT/customReact/customreact.js
+++ REACT/customReact/customreact.js
@@ -1,16 +1,26 @@
 function customRender(reactElement, mainContainer) {
     // Step 1: Create a DOM element based on the type specified in reactElement
-    const domElement = document.createElement(reactElement.type);
+    // const domElement = document.createElement(reactElement.type);
 
-    // Step 2: Set the inner HTML of the DOM element to the children of reactElement
-    domElement.innerHTML = reactElement.children;
+    // // Step 2: Set the inner HTML of the DOM element to the children of reactElement
+    // domElement.innerHTML = reactElement.children;
 
-    // Step 3: Set attributes for the DOM element using the props from reactElement
-    domElement.setAttribute('href', reactElement.props.href);
-    domElement.setAttribute('target', reactElement.props.target);
+    // // Step 3: Set attributes for the DOM element using the props from reactElement
+    // domElement.setAttribute('href', reactElement.props.href);
+    // domElement.setAttribute('target', reactElement.props.target);
 
-    // Step 4: Append the created DOM element to the mainContainer
-    mainContainer.appendChild(domElement);
+    // // Step 4: Append the created DOM element to the mainContainer
+    // mainContainer.appendChild(domElement);
+
+    // more modular code no repetition
+
+    const domElement = document.createElement(reactElement.type)
+    domElement.innerHTML = reactElement.children
+    for (const prop in reactElement.props) {
+        if (prop === 'children') continue ;
+        domElement.setAttribute(prop , reactElement.props[prop])
+    }
+    mainContainer.appendChild(domElement)
 }
 
 const reactElement = {
