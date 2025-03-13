52c352946d2286023ab4ea15c33d6b3112c5f27e
Muhammad Husnain <husnainbhinder682@gmail.com>
2025-03-14 03:58:57 +0500

    updated files

--- REACT/reactVite-p6-currencyConverter/src/hooks/useCurrencyInfo.js
+++ REACT/reactVite-p6-currencyConverter/src/hooks/useCurrencyInfo.js
@@ -4,7 +4,9 @@ function useCurrencyInfo(currency) {
 const[data,setData] = useState({})
     useEffect(() => {
         // Fetch currency data when the `currency` prop changes
-        fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`)
+        fetch(
+            `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`
+        )
         .then((res)=>res.json())
     .then((res)=>setData(res)[currency])
     console.log(data);
