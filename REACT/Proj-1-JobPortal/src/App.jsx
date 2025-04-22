b47de0cd8af7b6527584c1ef29322f3bfffa5a8d
Muhammad Husnain <husnainbhinder682@gmail.com>
2025-04-22 12:12:57 +0500

    updated  files

--- REACT/Proj-1-JobPortal/src/App.jsx
+++ REACT/Proj-1-JobPortal/src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
 import HomeCards from "./components/HomeCards";
 import JobListings from "./components/JobListings"
+import ViewAllJobs from "./components/ViewAllJobs";
 
 import "./index.css"; // Make sure this imports your Tailwind CSS
 
@@ -16,6 +17,7 @@ const App = () => {
       <Hero title="Become a React Dev" subtitle="Find the React job that fits your skills and needs" />
       <HomeCards />
       <JobListings/>
+      <ViewAllJobs/>
 
       {/* <!-- Hero --> */}
 
@@ -24,14 +26,7 @@ const App = () => {
       {/* <!-- Browse Jobs --> */}
 
       
-      <section className="m-auto max-w-lg my-10 px-6">
-        <a
-          href="jobs.html"
-          className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
-        >
-          View All Jobs
-        </a>
-      </section>
+     
     </>
   );
 };
