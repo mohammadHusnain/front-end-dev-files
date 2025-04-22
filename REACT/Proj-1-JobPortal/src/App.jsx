ebff895e3797739f5d71774392aee5c0c795fcde
Muhammad Husnain <husnainbhinder682@gmail.com>
2025-04-22 22:51:31 +0500

    updated files

--- REACT/Proj-1-JobPortal/src/App.jsx
+++ REACT/Proj-1-JobPortal/src/App.jsx
@@ -1,36 +1,25 @@
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
 import React from "react";
-import Navbar from "./components/Navbar"
-import Hero from "./components/Hero"
-import HomeCards from "./components/HomeCards";
-import JobListings from "./components/JobListings"
-import ViewAllJobs from "./components/ViewAllJobs";
+import HomePage from "./pages/HomePage";
+import MainLayout from "./layouts/MainLayout";
+
+
 
 import "./index.css"; // Make sure this imports your Tailwind CSS
 
-
+const router = createBrowserRouter(
+  createRoutesFromElements(<Route index element={<HomePage />} />)
+);
 
 const App = () => {
-
-  return (
-    <>
-      <Navbar />
-      <Hero title="Become a React Dev" subtitle="Find the React job that fits your skills and needs" />
-      <HomeCards />
-      <JobListings/>
-      <ViewAllJobs/>
-
-      {/* <!-- Hero --> */}
-
-      {/* <!-- Developers and Employers --> */}
-
-      {/* <!-- Browse Jobs --> */}
-
-      
-     
-    </>
-  );
+  return <RouterProvider router={router} />;
 };
 
+
+
 export default App;
-
-
---
+++ REACT/Proj-1-JobPortal/src/pages/HomePage.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Hero from "../components/Hero";
+
+const HomePage = () => {
+  return (
+    <div>
+      <Hero
+        title="Become A React Dev"
+        subtitle="Find A React Job That fits your skillset"
+      />
+    </div>
+  );
+};
+
+export default HomePage;
