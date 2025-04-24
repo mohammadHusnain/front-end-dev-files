4e0dcff587f6c0c3f36929b67c951abc8ec4f89b
Muhammad Husnain <husnainbhinder682@gmail.com>
2025-04-24 20:29:13 +0500

    updated files

--- REACT/Proj-1-JobPortal/src/App.jsx
+++ REACT/Proj-1-JobPortal/src/App.jsx
@@ -9,7 +9,8 @@ import HomePage from "./pages/HomePage";
 import MainLayout from "./layouts/MainLayout";
 import JobsPage  from "./pages/JobsPage";
 import NotFoundPage from "./pages/NotFoundPage";
-import JobPage from "./pages/JobPage";
+import JobPage, { jobLoader } from "./pages/JobPage";
+import AddJobPage from "./pages/AddJobPage";
 
 
 
@@ -22,7 +23,8 @@ const router = createBrowserRouter(
     
     <Route index element={<HomePage />} />
     <Route path='/jobs' element={<JobsPage />} />
-    <Route path='/jobs/:id' element={<JobPage />} />
+    <Route path='/add-job' element={<AddJobPage />} />
+    <Route path='/jobs/:id' element={<JobPage />} loader={jobLoader} />
     <Route path='*' element={<NotFoundPage />} />
     </Route>
 )
